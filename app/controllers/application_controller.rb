d645fb34d28cdacbf4285fb5e7f8b8c0dadfe1e8
sawara711 <uekibati.su@gmail.com>
2023-07-31 11:53:32 +0900

    rootルーティング実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  # 新規登録時のストロングパラメーターを設定
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :givenname, :surname_kana, :givenname_kana, :birthday])
   end

51deab816cf272f51f55dd34fa51c3935cea3ee8
sawara711 <uekibati.su@gmail.com>
2023-07-31 10:55:55 +0900

    エラーハンドリングに伴うバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # 仮想カラムpasswordへ英数字6文字以上必須のバリデーションを設定
+
+  # set errorm message
+  error_message_pass = 'is invalid. Include both letters and numbers'
+  error_message_fullwidth = 'is invalid. Input full-width characters'
+  error_message_kana = 'is invalid. Input full-width katakana characters'
+  # set valid
   VALID_PASSWORD_REGEX = /\A(?=.*?[A-Za-z])(?=.*?\d)[A-Za-z\d]{6,}\z/.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: '半角英数を両方含む必要があります'}
+  VALID_FULLWIDTH_CHAR = /\A[一-龯ぁ-んァ-ンー]+\z/.freeze
+  VALID_FULLWIDTH_CHAR_KANA = /\A[ァ-ンー]+\z/.freeze
+  
+  # 仮想カラムpasswordへ英数字6文字以上必須のバリデーションを設定
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: error_message_pass}
+
+  # 属性が空でな全角文字のみであることを検証
+  with_options presence: true, 
+               format: { with: VALID_FULLWIDTH_CHAR, message: error_message_fullwidth} do
+    validates :surname
+    validates :givenname
+  end
+
+  # 属性が空でなカナ文字のみであることを検証
+  with_options presence: true, 
+               format: { with: VALID_FULLWIDTH_CHAR_KANA, message: error_message_kana} do
+    validates :surname_kana
+    validates :givenname_kana
+  end
 
   # 属性が空でないことを検証
   with_options presence: true do
     validates :nickname
-    validates :surname
-    validates :givenname
-    validates :surname_kana
-    validates :givenname_kana
     validates :birthday  
   end
 end

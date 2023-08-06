9be5c625c8bb6aa63cc8febc67e9a8fb5e317223
sawara711 <uekibati.su@gmail.com>
2023-08-07 02:32:59 +0900

    可読性向上のため、モデルの記載方法変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,28 +12,23 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+\z/.freeze
   VALID_FULLWIDTH_CHAR = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   VALID_FULLWIDTH_CHAR_KANA = /\A[ァ-ヶー]+\z/.freeze
-  
-  
+
   # 仮想カラムpasswordが英数字6文字以上となることを検証
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: error_message_pass }
 
-  # 属性が空ではなく全角文字のみ(ー含む)となることを検証
-  with_options presence: true,
-               format: { with: VALID_FULLWIDTH_CHAR, message: error_message_fullwidth } do
-    validates :surname
-    validates :givenname
-  end
-
-  # 属性が空ではなくカナ文字のみ(ー含む)となることを検証
-  with_options presence: true,
-               format: { with: VALID_FULLWIDTH_CHAR_KANA, message: error_message_kana } do
-    validates :surname_kana
-    validates :givenname_kana
-  end
-
-  # 属性が空でないことを検証
+  # 属性が空であることを検証
   with_options presence: true do
     validates :nickname
     validates :birthday
+    # 全角文字のみ(ー含む)となることを検証
+    with_options format: { with: VALID_FULLWIDTH_CHAR, message: error_message_fullwidth } do
+      validates :surname
+      validates :givenname
+    end
+    # カナ文字のみ(ー含む)となることを検証
+    with_options format: { with: VALID_FULLWIDTH_CHAR_KANA, message: error_message_kana } do
+      validates :surname_kana
+      validates :givenname_kana
+    end
   end
 end

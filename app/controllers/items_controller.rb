fa42e753e7003595b16f34efa54806a833e58e3c
sawara711 <uekibati.su@gmail.com>
2023-08-06 21:57:55 +0900

    ItemController作成及びnew/index/createアクション記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :move_check, only: [ :new ]
+  
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(newitem_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+  def newitem_params
+    params.require(:item).permit(
+      :image, :title, :describe, :category_id, :condition_id, :price,
+      :shipping_charge_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_check
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+  
 end

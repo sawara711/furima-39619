f13924e9341d69a827946b7c546f8b1c2f46fa0a
sawara711 <uekibati.su@gmail.com>
2023-08-07 14:44:29 +0900

    binding.pry消し忘れ

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -85,7 +85,6 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Price is out of setting range")
         end
         it 'ユーザーが紐づいていいなければ登録できない' do
-          binding.pry
           @item.user = nil
           @item.valid?
           expect(@item.errors.full_messages).to include("User must exist")

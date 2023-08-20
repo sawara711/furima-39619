6e605e6ade3d18c7afb31e5476e5a88bf219ab38
sawara711 <uekibati.su@gmail.com>
2023-08-20 16:53:28 +0900

    エラーメッセージ修正

--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -9,8 +9,8 @@ class OrderDelivery
   error_message_blank = "can't be blank"
   error_message_postcode = "is invalid. Enter it as follows (e.g. 123-4567)"
   error_message_number = "is invalid. Input only number"
-  error_message_short = "is too short (message for short length)"
-  error_message_long = "is too long (message for long length)"
+  error_message_short = "is too short"
+  error_message_long = "is too long"
 
   # set valid
   VALID_POSTCODE = /\A[0-9]{3}[-][0-9]{4}\z/.freeze
@@ -27,13 +27,13 @@ class OrderDelivery
     # 桁数が指定範囲内且つ、半角数字のみであることを検証
     validates :phonenumber,
       numericality: { only_integer: true, message: error_message_number },
-      length: { minimum: 10, maximum: 11, too_short: error_message_short }
+      length: { minimum: 10, maximum: 11, too_short: error_message_short, too_long: error_message_long }
     end
   # 属性が1以外であることを検証
   validates :prefecture_id, exclusion: { in: [1], message: error_message_blank }
 
   def save
-    order = Order.create(item_id: item_id, user_id: user_id, token: token)
+    order = Order.create(item_id: item_id, user_id: user_id)
     Delivery.create(postcode: postcode, prefecture_id: prefecture_id, city: city, address: address,
                     building: building, phonenumber: phonenumber, order_id: order.id)
   end
